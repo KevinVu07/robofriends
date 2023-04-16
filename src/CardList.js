b1c3e292fa28f6f3f23cc49bceac255250117446
Kevin Vu <hongkhanhvu94@gmail.com>
2023-04-16 19:11:57 +1000

    first commit

---
+++ src/CardList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card.js";
+
+const CardList = ({ robots }) => {
+  const cardComponent = robots;
+  return (
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    </div>
+  );
+};
+
+export default CardList;
